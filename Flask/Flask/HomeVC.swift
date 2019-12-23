3e631acbb6101e6722bb6bbe86d505efca24c317
mohammed <mdrame113@gmail.com>
2019-12-22 23:59:26 -0800

    good'

--- Flask/Flask/HomeVC.swift
+++ Flask/Flask/HomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeVC: UIViewController, Data {
+class HomeVC: UIViewController {
  
     
     
@@ -16,19 +16,15 @@ class HomeVC: UIViewController, Data {
     
     var data : String?
     
-    func saveData(text: String) {
-        data = text
-        if let ava = data {
-                 label.text = ava
-                 print(ava)
-             }
-     }
+    
     
     @IBOutlet weak var label: UILabel!
     
     
     @IBAction func add(_ sender: Any) {
-        performSegue(withIdentifier: "toDeal", sender: self)
+        let vc = storyboard?.instantiateViewController(withIdentifier: "screen") as! DetailVC
+        vc.delegate = self
+        present(vc, animated: true, completion: nil)
     }
     
 
@@ -40,68 +36,15 @@ class HomeVC: UIViewController, Data {
     }
 
 
-//    // MARK: - Table view data source
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return list.count
-//    }
-//
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        cell.textLabel?.text = list[indexPath.row].data
-//        return cell
-//    }
-//
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "toDeal", sender: self)
-//    }
+
+
+}
+
+
+extension HomeVC: Data {
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func saveData(text: String) {
+        label.text = text
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
